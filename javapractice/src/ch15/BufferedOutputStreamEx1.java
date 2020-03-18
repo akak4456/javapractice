36072f8210c40c55d01af4b2f485dc324aef224e
akak4456 <akak4456@naver.com>
2020-03-18 20:41:10 +0900

    2020.03.18

---
+++ javapractice/src/ch15/BufferedOutputStreamEx1.java
@@ -0,0 +1,24 @@
+package ch15;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferedOutputStreamEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileOutputStream fos = new FileOutputStream("123.txt");
+			BufferedOutputStream bos = new BufferedOutputStream(fos,5);
+			
+			for(int i='1';i<='9';i++) {
+				bos.write(i);
+			}
+			bos.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
